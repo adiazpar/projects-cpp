5abd8575ef9ccfaa1920f1ee491a712bc80a2bff
StonyFever1 <alexdiaz0923@gmail.com>
2023-08-23 08:19:07 -0600

    Update hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Function prototype
+
+int main()
+{
+    // Variables
+
+    // Get the object's mass
+
+    // Get the object's velocity
+
+    // Display the objects kinetic energy
+
+    return 0;
+}
